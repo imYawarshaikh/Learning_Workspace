e2e04d6736efb47d7aa21d4d3e8d4dac4ed97df7
imYawarshaikh <yawarshaikh26@gmail.com>
2017-04-21 15:27:43 +0530

    Add files via upload
    Log4j Hello World!!

---
+++ LogTest/src/com/a/a/Testt.java
@@ -0,0 +1,18 @@
+package com.a.a;
+
+import org.apache.log4j.Logger;
+
+public class Testt {
+	
+	
+final static Logger logger= Logger.getLogger(Testt.class);
+   public  static void main(String[] args){
+	   
+	   
+	   
+	   logger.info("hi i m Yawar");
+	   logger.warn("hi i m Yawar");
+	   
+   }
+	
+}
